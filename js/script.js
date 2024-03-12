1ab0311796e15e35824b4dfda687d5b81acb5657
himanshubundell10 <himanshubundel10@gmail.com>
2024-03-13 00:31:20 +0530

    Added htaccess file

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ async function displayAlbums() {
     for (let index = 0; index < array.length; index++) {
         const e = array[index];
         
-          if (e.href.includes("/songs")) {
+          if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0];
             console.log(folder);                        
             //get the meta data of the folder
